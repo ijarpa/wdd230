c812bdcd36ec94f24f91cbf08d2e9f31571fd4b5
Ivan Jarpa <ivanandres.jarpa@gmail.com>
2023-05-06 23:39:47 -0400

    Assignment w03

--- lesson3/scripts/script.js
+++ lesson3/scripts/script.js
@@ -1,5 +1,26 @@
-const date = new Date();
-const year = date.getFullYear();
-const copyright = document.querySelector('footer p');
+const d = new Date();
+const year = d.getFullYear();
+console.log(year);
+document.querySelector(".currentYear").textContent = year;
 
+
+// Last Updated
+let lastModif = new Date(document.lastModified);
+let dayModif = lastModif.toLocaleString("en-US", {day: "numeric"});
+let monthModif = lastModif.toLocaleString("en-US", {month: "numeric"});
+let yearModif = lastModif.getFullYear();
+let hoursModif = lastModif.getHours();
+let minutesModif = lastModif.getMinutes();
+let secondsModif = lastModif.getSeconds();
+
+let fullDateModif = `${monthModif}/${dayModif}/${yearModif} ${hoursModif}:${minutesModif}:${secondsModif}`;
+
+console.log(lastModif);
+console.log(dayModif);
+console.log(monthModif);
+document.querySelector(".date-modification").textContent = fullDateModif;
+
+
+const date = new Date();
+const copyright = document.querySelector('footer p');
 copyright.innerHTML = `&copy; Southern Chile Chamber, ${year}`;
