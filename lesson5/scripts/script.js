b8789176edd70683707dc387815cab4eb45e287b
ijarpa <ivanandres.jarpa@gmail.com>
2023-05-19 19:22:14 -0400

    BOM finished

--- lesson5/scripts/script.js
+++ lesson5/scripts/script.js
@@ -1,39 +1,27 @@
-// Get necessary elements
-const input = document.getElementById('favchap');
-const button = document.getElementById('button');
-const list = document.getElementById('list');
+// Variables
+const input = document.querySelector('#favchap');
+const addBtn = document.querySelector('#button');
+const list = document.querySelector('#list');
 
-// Click event listener for the "Add Chapter" button
-button.addEventListener('click', function() {
-    // Check if input is not blank
-    if (input.value.trim() !== '') {
-    // Create list item (li) element
-    const listItem = document.createElement('li');
-
-    // Create delete button
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = '❌';
-
-    // Set li element's textContent with input value
-    listItem.textContent = input.value;
-
-    // Append delete button to li element
-    listItem.appendChild(deleteBtn);
-
-    // Append li element to the list
-    list.appendChild(listItem);
-
-    // Event listener for the delete button
-    deleteBtn.addEventListener('click', function() {
-        // Remove the li element when clicked
-        listItem.remove();
-
-        // Set focus back to the input element
+document.addEventListener('DOMContentLoaded', function() {
+    addBtn.addEventListener('click', function() {
+      if (input.value.trim() !== '') {
+        const listItem = document.createElement('li');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = '❌';
+  
+        listItem.textContent = input.value;
+        listItem.appendChild(deleteBtn);
+        list.appendChild(listItem);
+  
+        deleteBtn.addEventListener('click', function() {
+          listItem.remove();
+          input.focus();
+        });
+  
+        input.value = '';
         input.focus();
+      }
     });
-
-    // Clear the input value and set focus to the input element
-    input.value = '';
-    input.focus();
-    }
-});
+  });
+  
