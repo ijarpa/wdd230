62641829f7ae50d83793b7807d846a468bc69b7c
Ivan Jarpa <ivanandres.jarpa@gmail.com>
2023-06-14 11:29:21 -0400

    update

---
+++ lesson9/scripts/testAPI.js
@@ -0,0 +1,12 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
+  }
+  
+  getProphetData();
+
+
+
